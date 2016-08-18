20a46800e41fe99b32df59c3748c7107df7eaa39
zhuyi01 <zhuyi01@lianjia.com>
2016-08-18 10:51:59 +0800

    补充运算符

---
+++ src/operations/custom.js
@@ -0,0 +1,24 @@
+import {objForeach, getType} from '../utils'
+
+export default objForeach({
+
+  /**
+   * [exec 自定义执行]
+   * @param  {[not obejct]}     target 之前的属性
+   * @param  {[function ]}      [fn]   处理函数，处理target为return 值，但是注意，处理完必须和之前的类型一致
+   * @return {[same as target]}           [description]
+   */
+  exec: function (target, [fn]) { 
+    const type = getType(target)
+    if (getType(target) === 'object') throw '$exec 必须不是个对象才可以'
+    if (getType(fn) != 'function') throw '$exec 参数必须是一个function'
+    if (type === 'array') target = [].concat(target)
+
+    let result = fn.call(null, target) 
+    if (type === getType(result)) return result
+    throw '$exec中, 处理函数必须入参和返回值类型一致'
+  }
+
+}, (operation) => {
+  return (target, args) => operation(target, args === undefined ? [] : [].concat(args))
+})
