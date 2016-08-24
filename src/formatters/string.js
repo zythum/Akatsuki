e3481482912f108ea739cea37d878e5994faa7a0
zhuyi01 <zhuyi01@lianjia.com>
2016-08-24 13:10:54 +0800

    加了一些的注释

--- src/formatters/string.js
+++ src/formatters/string.js
@@ -1,16 +1,25 @@
 import {objForeach, getType, toStr} from '../utils'
 
+
+function _apply (instance, methodName, args) {
+  return instance['methodName'].apply(instance, args)
+}
+
 export default objForeach({
 
-  replace (string, args) { return string.replace.apply(string, args) },
-  substr (string, args) { return string.substr.apply(string, args) },
-  substring (string, args) { return string.substring.apply(string, args) },
-  slice (string, args) { return string.slice.apply(string, args) },
-  trim (string, args) { return string.trim() },
-  trimLeft (string, args) { return string.trimLeft() },
+  // string自有方法
+  replace   (string, args) { return _apply(string, 'replace',   args) },
+  substr    (string, args) { return _apply(string, 'substr',    args) },
+  substring (string, args) { return _apply(string, 'substring', args) },
+  slice     (string, args) { return _apply(string, 'slice',     args) },
+  
+  trim      (string, args) { return string.trim() },
+  trimLeft  (string, args) { return string.trimLeft() },
   trimRight (string, args) { return string.trimRight() },
 
+  // string前添加字符串
   prefix (string, [arg]) { return '' + arg + string },
+  // string后添加字符串
   suffix (string, [arg]) { return '' + string + arg },
 
 }, (formatter, name, object) => {
