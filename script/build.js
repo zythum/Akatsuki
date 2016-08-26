adc4c5fb1118254d18275ce9e8e23f257bc8292a
zhuyi01 <zhuyi01@lianjia.com>
2016-08-26 16:50:03 +0800

    build 时显示文件大小

--- script/build.js
+++ script/build.js
@@ -1,17 +1,19 @@
-var rollup = require('rollup')
-var babel = require('rollup-plugin-babel')
-var uglify = require('rollup-plugin-uglify')
+const fs = require('fs')
+const zlib = require('zlib')
+const rollup = require('rollup')
+const babel = require('rollup-plugin-babel')
+const uglify = require('rollup-plugin-uglify')
+const formateSize = s => (s/1024).toFixed(2)
 
-var logText = 'write /dist/akatsuki.js'
-var logTextMin = 'write /dist/akatsuki.min.js'
 
-console.time(logText)
+console.log('> Build Akatsuki <')
+
+let startAt = Date.now()
 rollup.rollup({
   entry: './src/index.js',
   plugins: [ babel() ],
 })
 .then( bundle => {
-  console.timeEnd(logText)
   return bundle.write({
     format: 'umd',
     moduleName: 'Akatsuki',
@@ -19,15 +21,26 @@ rollup.rollup({
     sourceMap: true
   })
 })
+.then(()=> {
+  return new Promise(resolve => {
+    const time = Date.now() - startAt
+    const code = fs.readFileSync('./dist/akatsuki.js')
+    console.log(
+      'Write akatsuki.js',
+      `Time: ${time}ms`,
+      `Size: ${formateSize(code.length)}kb`
+    )
+    startAt = Date.now()
+    resolve()
+  })
+})
 .then(() => {
-  console.time(logTextMin)
   return rollup.rollup({
     entry: './src/index.js',
     plugins: [ babel(), uglify() ],
   })
 })
 .then( bundle => {
-  console.timeEnd(logTextMin)
   return bundle.write({
     format: 'umd',
     moduleName: 'Akatsuki',
@@ -35,5 +48,19 @@ rollup.rollup({
     sourceMap: true
   })
 })
+.then(()=> {
+  return new Promise(resolve => {
+    const time = Date.now() - startAt
+    const code = fs.readFileSync('./dist/akatsuki.min.js')
+    const gzipSize = zlib.gzipSync(code, {level: 9}).length
+    console.log(
+      'Write akatsuki.min.js',
+      `Time: ${time}ms`,
+      `Size: ${formateSize(code.length)}kb ${formateSize(gzipSize)}kb(gzip)`
+    )
+    startAt = Date.now()
+    resolve()
+  })
+})
 .catch( e => console.log('got errors:', e) )
-console.log('build Akatsuki')
+
