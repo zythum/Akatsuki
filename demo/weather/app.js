df998daac01c9ae706c99dbba4d10418a5949827
zhuyi01 <zhuyi01@lianjia.com>
2016-09-01 11:22:48 +0800

    添加天气demo

---
+++ demo/weather/app.js
@@ -0,0 +1,75 @@
+var apiURL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22[city]%2C%20ak%22)&format=json'
+var speedMap = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117]
+
+function jsonp (url, callback) {
+  var count = jsonp.count = jsonp.count || 0
+  var handle = 'jsonp' + count++
+  var script = document.createElement('script')
+  window[handle] = function (arg) {
+    delete window[handle]
+    document.body.removeChild(script)
+    callback(arg)
+  }
+  document.body.appendChild(script).src = url + '&callback=' +handle
+}
+
+function pointToPath (points) {
+  var min = Infinity
+  var max = 0
+
+  points.forEach(function (point) {
+    min = Math.min(min, point[0], point[1])
+    max = Math.max(max, point[0], point[1])
+  })
+
+  var path = ['', '']
+  points.forEach(function (point, index) {
+    var prefix = (index == 0 ? 'M' : 'L') + (index * 72 + 36) + ','
+    path.forEach(function (_, i) {
+      path[i] += prefix + (((point[i] - min) / (max - min) * 110) + 20)
+    })
+  })
+  return path
+}
+
+var app = Akatsuki(document.body, {
+  viewDidMount: function () {
+    this.load()
+    this.interval = setInterval(this.load, 60000)
+    window.addEventListener('hashchange', this.load)
+  },
+  viewWillUnmout: function () {
+    window.removeEventListener('hashchange', this.load)
+    clearInterval(this.interval)
+    delete this.interval
+  },
+  formatters: {
+    weather: function (value) { return weathers[value] },
+    celsius: function (value) { return ((value - 32) / 1.8).toFixed(0) },
+    windBeaufort: function (value) {
+      for (var i = 0, len = speedMap.length; i < len; i++) {
+        if (value < speedMap[i]) return i
+      }
+      return i
+    }
+  },
+  computed: {
+    forecastPath: ['item.forecast', function (forecast) {
+      if (forecast) 
+        return pointToPath(forecast.map(function (one) { 
+          return [parseInt(one.high), parseInt(one.low)] 
+        }))
+    }]
+  },
+  methods: {
+    load: function () {
+      var self = this
+      var href = location.href.split('#')
+      var city = href[1]
+      if (!city) return location.replace(href[0] + '#beijing')
+      jsonp(apiURL.replace('[city]', city), function (data) {
+        self.update('$set', data.query.results.channel)
+      })
+    }
+  }
+})
---
+++ demo/weather/icons.map.js
@@ -0,0 +1,51 @@
+var weathers = {
+ 0: '\uf056', //wi-tornado                  tornado
+ 1: '\uf073', //wi-hurricane                tropical storm
+ 2: '\uf073', //wi-hurricane                hurricane
+ 3: '\uf016', //wi-lightning                severe thunderstorms
+ 4: '\uf01e', //wi-thunderstorm             thunderstorms
+ 5: '\uf017', //wi-rain-mix                 mixed rain and snow
+ 6: '\uf017', //wi-rain-mix                 mixed rain and sleet
+ 7: '\uf0b5', //wi-sleet                    mixed snow and sleet
+ 8: '\uf0b5', //wi-sleet                    freezing drizzle
+ 9: '\uf019', //wi-rain                     drizzle
+10: '\uf019', //wi-rain                     freezing rain
+11: '\uf01a', //wi-showers                  showers
+12: '\uf01a', //wi-showers                  showers
+13: '\uf01b', //wi-snow                     snow flurries
+14: '\uf01b', //wi-snow                     light snow showers
+15: '\uf01b', //wi-snow                     blowing snow
+16: '\uf01b', //wi-snow                     snow
+17: '\uf015', //wi-hail                     hail
+18: '\uf0b5', //wi-sleet                    sleet
+19: '\uf063', //wi-dust                     dust
+20: '\uf014', //wi-fog                      foggy
+21: '\uf0b6', //wi-day-haze                 haze
+22: '\uf062', //wi-smoke                    smoky
+23: '\uf085', //wi-day-windy                blustery
+24: '\uf0c4', //wi-day-light-wind           windy
+25: '\uf076', //wi-snowflake-cold           cold
+26: '\uf013', //wi-cloudy                   cloudy
+27: '\uf086', //wi-night-alt-cloudy         mostly cloudy (night)
+28: '\uf002', //wi-day-cloudy               mostly cloudy (day)
+29: '\uf081', //wi-night-alt-partly-cloudy  partly cloudy (night)
+30: '\uf00c', //wi-day-sunny-overcast       partly cloudy (day)
+31: '\uf02e', //wi-night-clear              clear (night)
+32: '\uf00d', //wi-day-sunny                sunny
+33: '\uf02e', //wi-night-clear              fair (night)
+34: '\uf00d', //wi-day-sunny                fair (day)
+35: '\uf032', //wi-night-hail               mixed rain and hail
+36: '\uf072', //wi-hot                      hot
+37: '\uf01e', //wi-thunderstorm             isolated thunderstorms
+38: '\uf01e', //wi-thunderstorm             scattered thunderstorms
+39: '\uf01e', //wi-thunderstorm             scattered thunderstorms
+40: '\uf01a', //wi-showers                  scattered showers
+41: '\uf01a', //wi-showers                  heavy snow
+42: '\uf01a', //wi-showers                  scattered snow showers
+43: '\uf01a', //wi-showers                  heavy snow
+44: '\uf00c', //wi-day-sunny-overcast       partly cloudy
+45: '\uf00e', //wi-day-storm-showers        thundershowers
+46: '\uf01a', //wi-showers                  snow showers
+47: '\uf00e', //wi-day-storm-showers        isolated thundershowers
+3200: '\uf07b'//wi-na                       not available
+}
